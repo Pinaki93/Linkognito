d66d59f078e19ff4c06f52e7617db32e9b560a51
pinakiacharya <pinakiacharyagd@gmail.com>
2025-08-08 19:07:20 +0530

    Initial commit: Android project setup with comprehensive .gitignore

---
+++ app/src/main/java/dev/pinaki/linkognito/MainActivity.java
@@ -0,0 +1,12 @@
+package dev.pinaki.linkognito;
+
+import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
